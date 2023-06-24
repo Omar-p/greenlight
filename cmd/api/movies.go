d606e753e9706f21335861620640f57e2ed02756
Omar Shabaan <omar.plus.077@gmail.com>
2023-06-24 16:16:48 +0300

    creating Movie type and use it to show movie info.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"greenlight/internal/data"
 	"net/http"
+	"time"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint.
@@ -17,5 +19,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, movie, nil); err != nil {
+		app.logger.Print(err)
+		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
