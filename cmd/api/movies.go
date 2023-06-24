54fb616e03680d3968e7922d45aac39945ed599c
Omar Shabaan <omar.plus.077@gmail.com>
2023-06-24 11:58:06 +0300

    adding helper to read id param from the request and convert it to int64 and using it in showMovieHandler.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint.
@@ -14,13 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
