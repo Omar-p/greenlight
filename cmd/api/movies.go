c0707633b00d02e6c82a7d2e14e3d307b440da96
Omar Shabaan <omar.plus.077@gmail.com>
2023-06-25 12:55:55 +0300

    update createMovieHandler to accept input from request body.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,7 +9,19 @@ import (
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	app.logger.Print(fmt.Sprintf("%+v", input))
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint.
