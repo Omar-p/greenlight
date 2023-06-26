b63959686b58a48872382528f79b4743b3b6c1f0
Omar Shabaan <omar.plus.077@gmail.com>
2023-06-26 12:27:55 +0300

    adding validation rules for title, genres, year, and runtime in data.Movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"greenlight/internal/validator"
+	"time"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -10,4 +13,37 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	validateTitle(v, movie)
+
+	validateYear(v, movie)
+
+	validateRuntime(v, movie)
+
+	validateGenres(v, movie)
+}
+
+func validateGenres(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+func validateRuntime(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+}
+
+func validateYear(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+}
+
+func validateTitle(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
 }
